212edf8836850c78eb1e4d6951b18cabc0e00b10
frougs <eeveetrainer51@gmail.com>
2023-03-12 20:08:30 -0400

    New character controller

---
+++ Assets/Scripts/Player Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public PlayerInputActions playerControls;
+    private InputAction move;
+    Vector3 moveInput;
+    Vector3 moveDirection;
+    Rigidbody rb;
+    private CharacterController controller;
+    [SerializeField] float playerSpeed = 10.0f;
+    [SerializeField] float rotateSpeed = 4f;
+    private Transform cameraMainTransform;
+
+
+    private void Start(){
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+        controller = gameObject.GetComponent<CharacterController>();
+        cameraMainTransform = Camera.main.transform;
+    }
+
+    void Awake(){
+        playerControls = new PlayerInputActions();
+    }
+
+    void Update(){
+        moveInput = move.ReadValue<Vector2>();
+        moveDirection = new Vector3(moveInput.x, 0, moveInput.y).normalized;
+        moveDirection = cameraMainTransform.forward * moveDirection.z + cameraMainTransform.right * moveDirection.x;
+        moveDirection.y = 0f;
+
+        if(moveInput != Vector3.zero){
+            float targetAngle = Mathf.Atan2(moveInput.x, moveInput.y) * Mathf.Rad2Deg +cameraMainTransform.eulerAngles.y;
+            Quaternion rotation = Quaternion.Euler(0f, targetAngle, 0f);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * rotateSpeed);
+        }
+
+    }
+
+    void FixedUpdate(){
+        controller.Move(moveDirection * Time.deltaTime * playerSpeed );
+    }
+
+    private void OnEnable(){
+        move = playerControls.Player.Move;
+        move.Enable();
+    }
+
+    private void OnDisable(){
+        move.Disable();
+    }
+}
