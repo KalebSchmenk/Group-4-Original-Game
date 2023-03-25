27afd2c985a1191d104563df33f7d472e722b927
frougs <eeveetrainer51@gmail.com>
2023-03-25 18:39:34 -0400

    jumping and health

--- Assets/Scripts/Player Scripts/PlayerController.cs
+++ Assets/Scripts/Player Scripts/PlayerController.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     public PlayerInputActions playerControls;
     private InputAction move;
+    private InputAction jump;
     Vector3 moveInput;
     Vector3 moveDirection;
     Rigidbody rb;
@@ -25,6 +27,17 @@ public class PlayerController : MonoBehaviour
     private bool onGround;
 
     private float verticalVelocity;
+    [SerializeField] float jumpForce = 1.0f;
+    private float impactTimer;
+    private float jumpSpeed;
+    [SerializeField] private float maxHealth = 100;
+    private float currentHeath;
+    public Image healthMask;
+    float normSize;
+    bool isInvincible;
+    [SerializeField] float invincibilityFramesDuration = 1.5f;
+     
+    
     
 
 
@@ -33,6 +46,8 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         controller = gameObject.GetComponent<CharacterController>();
         cameraMainTransform = Camera.main.transform;
+        currentHeath = maxHealth;
+        normSize = healthMask.rectTransform.rect.width;
     }
 
     void Awake()
@@ -54,12 +69,15 @@ public class PlayerController : MonoBehaviour
             Debug.Log("GAME QUIT");
         }
 
-        onGround = controller.isGrounded;
+        
 
+        //Walking Stuff
         moveInput = move.ReadValue<Vector2>();
         moveDirection = new Vector3(moveInput.x, 0, moveInput.y);
         moveDirection = cameraMainTransform.forward * moveDirection.z + cameraMainTransform.right * moveDirection.x;
         moveDirection.y = 0f;
+
+
         
 
         if(onGround == true && verticalVelocity < 0){
@@ -67,12 +85,45 @@ public class PlayerController : MonoBehaviour
         }
         verticalVelocity += gravityValue * Time.deltaTime;
         
-        moveDirection.y = verticalVelocity;
+        //moveDirection.y = verticalVelocity;
 
         Quaternion rotation = Quaternion.Euler(0f, cameraMainTransform.eulerAngles.y, 0f);
         transform.rotation = rotation;
 
-        controller.Move(moveDirection.normalized * playerSpeed * Time.deltaTime);
+        float magnitude = Mathf.Clamp01(moveDirection.magnitude) * playerSpeed;
+
+
+        //Jumping Stuff
+        
+        onGround = controller.isGrounded;
+        if(onGround){
+            impactTimer = 0.2f;
+        }
+
+        if(impactTimer > 0){
+            impactTimer -= Time.deltaTime;
+        }
+
+        if(jump.triggered){
+            if(impactTimer > 0){
+                impactTimer = 0;
+                verticalVelocity += Mathf.Sqrt(jumpForce * 1.75f * Mathf.Abs(gravityValue));
+
+            }
+        }
+
+        
+        
+
+        //What actually moves the player
+        moveDirection = moveDirection.normalized;
+        Vector3 moving = moveDirection * magnitude;
+        
+        moving.y = verticalVelocity;
+        
+        //controller.Move(moveDirection.normalized * playerSpeed * Time.deltaTime);
+        controller.Move(moving * playerSpeed * Time.deltaTime);
+
 
 
 
@@ -89,6 +140,17 @@ public class PlayerController : MonoBehaviour
         //transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * rotateSpeed);
         //transform.rotation = rotation;
 
+        //Updates Health UI element
+        HealthVisual(currentHeath/maxHealth);
+
+        if(currentHeath <= 0){
+            Debug.Log("Dead");
+        }
+
+        //Debug.Log("Current HP: " +currentHeath +"/" + maxHealth);
+
+
+
     }
 
     void FixedUpdate()
@@ -100,12 +162,15 @@ public class PlayerController : MonoBehaviour
     private void OnEnable()
     {
         move = playerControls.Player.Move;
+        jump = playerControls.Player.Jump;
+        jump.Enable();
         move.Enable();
     }
 
     private void OnDisable()
     {
         move.Disable();
+        jump.Enable();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -115,7 +180,40 @@ public class PlayerController : MonoBehaviour
         // fireball damage is from a fireball and should do midrange damage, and lightning strike damage is from a lightning strike and should do a decent amount of damage
         if (other.gameObject.CompareTag("DamageCollider") || other.gameObject.CompareTag("Fireball") || other.gameObject.CompareTag("LightningStrike"))
         {
-            Debug.Log("Took damage!");
+            //Debug.Log("Took damage!");
+            if(other.gameObject.CompareTag("DamageCollider")){
+                if(isInvincible == false){
+                    currentHeath -= 10;
+                    StartCoroutine(InvincibilityFrames());
+                }
+            }
+            if(other.gameObject.CompareTag("Fireball")){
+                if(isInvincible == false){
+                    currentHeath -= 30;
+                    StartCoroutine(InvincibilityFrames());
+                }
+            }
+            if(other.gameObject.CompareTag("LightningStrike")){
+                if(isInvincible == false){
+                    currentHeath -= 50;
+                    StartCoroutine(InvincibilityFrames());
+                }
+            }
         }
     }
+
+    //Controls health UI element
+    public void HealthVisual(float amount){
+        healthMask.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, normSize * amount);
+    }
+
+    private IEnumerator InvincibilityFrames(){
+        Debug.Log("Invincible");
+        isInvincible = true;
+        yield return new WaitForSeconds(invincibilityFramesDuration);
+        isInvincible = false;
+        Debug.Log("Not Invincible");
+    }
+
+    
 }
